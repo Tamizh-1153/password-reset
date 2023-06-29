94c5359dca73ad16b75cfca0d747e88b648e818e
Tamizh-1153 <tamizh1153@gmail.com>
2023-06-29 09:28:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,18 @@ import "./App.css"
 import ResetPassword from './ResetPassword'
 import ForgotPassword from "./ForgotPassword"
 import Login from "./Login"
-import Register from "./Register"
 
 
 function App() {
-  return (
+  return(
     <BrowserRouter>
-      <Routes>
-        <Route path="password-reset-tm.netlify.app" Component={Register} />
-        <Route path="password-reset-tm.netlify.app/login" Component={Login} />
-        <Route
-          path="password-reset-tm.netlify.app/forgot-password"
-          Component={ForgotPassword}
-        />
-        <Route
-          path="password-reset-tm.netlify.app/api/v1/reset_password/:id/:token"
-          Component={ResetPassword}
-        />
-      </Routes>
+    <Routes>
+
+      <Route path="/" Component={Register} />
+      <Route path="/login" Component={Login} />
+      <Route path="/forgot-password" Component={ForgotPassword} />
+      <Route path="/api/v1/reset_password/:id/:token" Component={ResetPassword} />
+    </Routes>
     </BrowserRouter>
   )
 }
